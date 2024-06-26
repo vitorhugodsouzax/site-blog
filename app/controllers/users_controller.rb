66b34c62bb6f82cd88f8227ce58717fec7c9bae6
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-06-26 19:58:24 -0300

    refatoring code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,30 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-
+  
     def new
       @user = User.new
+    end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:notice] = "Your account information was successfully updated"
+        redirect_to @user
+      else
+        render 'edit'
+      end
     end
 
     def create
       @user = User.new(user_params)
       if @user.save
         flash[:notice] = "Welcome to the BlogSfera #{@user.username}, you have successfully signed up"
-        redirect_to articles_path
-      else
+        redirect_to @user
         render 'new'
       end
     end
