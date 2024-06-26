3b01e160f6fdf3f572a8c5e7d620605b28f79256
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-06-26 19:33:58 -0300

    add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 
 class UsersController < ApplicationController
 
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
     def new
       @user = User.new
     end
@@ -8,7 +14,7 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
-        flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+        flash[:notice] = "Welcome to the BlogSfera #{@user.username}, you have successfully signed up"
         redirect_to articles_path
       else
         render 'new'
