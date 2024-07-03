a9bbfeb56cfcb0a23019830da94dfd49a75d501a
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-07-03 17:36:10 -0300

    if login logout button pops up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)  end
 
   def index
@@ -31,6 +30,7 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
+        session[:user_id] = @user.id
         flash[:notice] = "Welcome to the BlogSfera #{@user.username}, you have successfully signed up"
         redirect_to @user
         render 'new'
@@ -40,6 +40,10 @@ class UsersController < ApplicationController
     private
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+    
+    def set_user
+      @user = User.find(params[:id])
     end
     
   end
