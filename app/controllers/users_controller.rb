deb24adca215cbb36d593476d25367dc68216e4d
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-07-04 17:32:11 -0300

    admin user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil if current_user == @user
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Conta e artigos associados foram deletados com sucesso!."
     redirect_to articles_path
   end
