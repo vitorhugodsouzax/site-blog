a2ed29e436a70cedf1f88c6d622af69f24fdcb4f
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-07-08 19:40:11 -0300

    rails g test_unit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       flash[:notice] = "Logged in successfully"
       redirect_to user
     else
-      flash.now[:alert] = "There was something wrong with your login details"
+      flash.now[:alert] = "Você errou a senha ou o email"
       render 'new'
     end
   end
